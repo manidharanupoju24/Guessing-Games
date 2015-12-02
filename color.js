13d83018d8c903a11e6cbf674f2aa7b1be5d40e4
uchihaitachi24 <manidharanupoju@gmail.com>
2015-12-02 23:18:58 +0530

    initial commit

---
+++ color.js
@@ -0,0 +1,13 @@
+var finished = false;
+var input_guess;
+
+function do_game(){
+
+	while(!finished){
+		input_guess = prompt("I am thinking of one of these colors:\n\n" + "blue, cyan, gold, gray, green, magenta, orange, red, white, yellow\n\n" + "What color am I thinking of?")
+		input = String(input_guess);
+
+
+	}
+
+}
